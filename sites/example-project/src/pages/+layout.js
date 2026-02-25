97984eff1b29a7e2c8c00ac151556a2d0cf0a916
yumin2002 <94658575+yumin2002@users.noreply.github.com>
2026-02-24 19:07:55 -0600

    Revert "refactor(site): replace example-project with runtime dashboard site"
    This reverts commit 86e1bf95bb5ab3d303285a99e530930a37dd7ef7.

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -89,7 +89,9 @@ export const load = async ({ fetch, route, params, url }) => {
 	const [{ customFormattingSettings }, pagesManifest, evidencemeta] = await Promise.all([
 		fetch(addBasePath('/api/customFormattingSettings.json/GET.json')).then((x) => x.json()),
 		fetch(addBasePath('/api/pagesManifest.json')).then((x) => x.json()),
-		Promise.resolve({ queries: [] })
+		fetch(addBasePath(`/api/${route.id}/evidencemeta.json`))
+			.then((x) => x.json())
+			.catch(() => ({ queries: [] }))
 	]);
 
 	const routeHash = md5(route.id);
---
+++ sites/legion-dashboard-site/src/pages/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let error;
+</script>
+
+<h1>Legion Dashboard Site</h1>
+<p>{error?.message ?? 'Unknown error'}</p>
